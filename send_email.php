a0049929e50d2892247d44eb415c01b01e90b19c
Lucas-Bracq <49907533+Lucas-Bracq@users.noreply.github.com>
2020-11-25 11:27:24 +0100

    HACKINK-EXTRA-POINT

--- send_email.php
+++ send_email.php
@@ -14,18 +14,18 @@ $mail = new PHPMailer(true);
 try {
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-    //$mail->isSMTP();                                            // Send using SMTP
-    $mail->Host = 'mail.ovh.net';                    // Set the SMTP server to send through
+    //$mail->isSMTP();                                          // Send using SMTP
+    $mail->Host = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth = true;                                   // Enable SMTP authentication
-    $mail->Username = 'VOTRE_LOGIN';                     // SMTP username
-    $mail->Password = 'PASSWORD';                               // SMTP password
-    //  $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
-    //  $mail->Port       = 587;                                    // TCP port to connect to
+    $mail->Username = 'chiquinha260@gmail.com';                     // SMTP username
+    $mail->Password = 'vanille91998';                               // SMTP password
+    $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+    $mail->Port       = 465;                                    // TCP port to connect to
 
     //Recipients
-    $mail->setFrom('newsletter@miw.ovh', 'MIW Party');
-    $mail->addAddress('your-name@mix.org', 'Leon');     // Add a recipient
-    $mail->addCC('newsletter@miw.ovh');
+    $mail->setFrom('chiquinha260@gmail.com', 'MIW Party');
+    var_dump($_POST['email']);
+    $mail->addAddress($_POST['email'], 'Leon');     // Add a recipient
 
     // Attachments
 //    $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
@@ -33,7 +33,7 @@ try {
 
     // Content
     $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Here is the subject';
+    $mail->Subject = 'Xmas Party !';
     $mail->Body = file_get_contents('email/email_party.html');
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
